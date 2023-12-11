2837e5dbed5894d82eb8d8c5cc6fa362ccac631f
andyrs313 <dande313@gmail.com>
2023-12-10 22:11:53 -0500

    Added grandstand and paddle control to pong

--- app/pong/page.tsx
+++ app/pong/page.tsx
@@ -33,6 +33,45 @@ const Wrapper = styled.div({
     backgroundColor: '#4e246c',
 });
 
+const PlayerPaddle = ({zPos, upKey, downKey}) => {
+    const [paddleX, setPaddleX] = useState(0);
+    const paddleRef = useRef<THREE.Mesh>(null);
+
+    useEffect(() => {
+        const handleKeyDown = (event) => {
+            switch(event.key) {
+                case upKey:
+                    setPaddleX(prevX => prevX - 4);
+                    break;
+                case downKey:
+                    setPaddleX(prevX => prevX + 4);
+                    break;
+                default:
+                    break;
+            }
+        };
+
+        window.addEventListener('keydown', handleKeyDown);
+
+        return () => {
+            window.removeEventListener('keydown', handleKeyDown);
+        };
+    }, []);
+
+    useFrame(() => {
+        if (paddleRef.current) {
+            paddleRef.current.position.x = paddleX;
+        }
+    });
+
+    return (
+        <mesh ref={paddleRef} position={[paddleX, 0, zPos]}>
+            <boxGeometry args={[PADDLE_WIDTH, PADDLE_HEIGHT, PADDLE_DEPTH]} />
+            <meshLambertMaterial color={0xffffff} />
+        </mesh>
+    );
+};
+
 const Paddle = ({zPos}) => {
     const paddleGeometry = new THREE.BoxGeometry(PADDLE_WIDTH, PADDLE_HEIGHT, PADDLE_DEPTH);
     const paddleMaterial = new THREE.MeshLambertMaterial({ color: 0xffffff });
@@ -73,15 +112,67 @@ const Ball = () => {
 };
 
 const Field = () => {
-    var fieldGeometry = new THREE.BoxGeometry(FIELD_WIDTH, 5, FIELD_LENGTH, 1, 1, 1),
-        fieldMaterial = new THREE.MeshLambertMaterial({ color: 0x003300 });
-        const field = new THREE.Mesh(fieldGeometry, fieldMaterial);
+    const fieldGeometry = new THREE.BoxGeometry(FIELD_WIDTH, 5, FIELD_LENGTH, 1, 1, 1),
+        fieldMaterial = new THREE.MeshLambertMaterial({ color: 0x008200 }),
+        field = new THREE.Mesh(fieldGeometry, fieldMaterial),
+        lineGeometry = new THREE.BoxGeometry(FIELD_WIDTH, 5, 5, 1, 1, 1),
+        lineMaterial = new THREE.MeshLambertMaterial({ color: 0xffffff }),
+        line = new THREE.Mesh(lineGeometry, lineMaterial);
+
+        line.position.set(0, -28, 0);
         field.position.set(0, -30, 0);
     return (
         <mesh>
             <primitive object={field} />
+            <primitive object={line} />
         </mesh>
     );
+};
+
+const FanStand = () => {
+    const fanStandAGeometry = new THREE.BoxGeometry(100, 50, FIELD_LENGTH),
+        fanStandAMaterial = new THREE.MeshLambertMaterial({ color: 0xcc0000 }),
+        fanStandA = new THREE.Mesh(fanStandAGeometry, fanStandAMaterial);
+    
+    //position fanstand at top of field
+    fanStandA.position.set((-FIELD_WIDTH / 2) - 50, 0, 0);
+
+    const fanStandBGeometry = new THREE.BoxGeometry(100, 100, FIELD_LENGTH),
+        fanStandBMaterial = new THREE.MeshLambertMaterial({ color: 0xee0000 }),
+        fanStandB = new THREE.Mesh(fanStandBGeometry, fanStandBMaterial);
+
+    //position behind fanstand A
+    fanStandB.position.set((-FIELD_WIDTH / 2) - 150, 25, 0);
+
+    //flags to each side of fanstand B
+    const flagPoleGeometry = new THREE.CylinderGeometry(6, 6, 100),
+        flagMaterial = new THREE.MeshLambertMaterial({ color: 0xaaaaaa }),
+        flagPoleA = new THREE.Mesh(flagPoleGeometry, flagMaterial),
+        flagPoleB = new THREE.Mesh(flagPoleGeometry, flagMaterial);
+    
+    flagPoleA.position.set((-FIELD_WIDTH / 2) - 150, 100, (FIELD_LENGTH / 2) - 10);
+    flagPoleB.position.set((-FIELD_WIDTH / 2) - 150, 100, -(FIELD_LENGTH / 2) + 10);
+
+    //Add waving flags to flagpoles
+    const flagGeometry = new THREE.BoxGeometry(2, 40, 55);
+    const flagMaterialA = new THREE.MeshBasicMaterial({color: 0x0000cc, side: THREE.DoubleSide});
+    const flagMaterialB = new THREE.MeshBasicMaterial({color: 0xd0d011, side: THREE.DoubleSide});
+    const flagA = new THREE.Mesh(flagGeometry, flagMaterialA);
+    const flagB = new THREE.Mesh(flagGeometry, flagMaterialB);
+    flagA.position.set((-FIELD_WIDTH / 2) - 150, 120, (FIELD_LENGTH / 2) - 40);
+    flagB.position.set((-FIELD_WIDTH / 2) - 150, 120, -(FIELD_LENGTH / 2) - 20);
+
+    return (
+        <mesh>
+            <primitive object={fanStandA} />
+            <primitive object={fanStandB} />
+            <primitive object={flagPoleA} />
+            <primitive object={flagPoleB} />
+            <primitive object={flagA} />
+            <primitive object={flagB} />
+        </mesh>
+    );
+
 };
 
 const Light = () => {
@@ -104,10 +195,11 @@ export default function Pong() {
             <Canvas camera={camera}>
                 <Light />
                 <OrbitControls />
-                <Paddle zPos={FIELD_LENGTH / 2}/>
+                <PlayerPaddle upKey="ArrowUp" downKey="ArrowDown" zPos={FIELD_LENGTH / 2}/>
                 <Paddle zPos={-FIELD_LENGTH / 2}/>
                 <Ball />
                 <Field />
+                <FanStand />
             </Canvas>
         </Wrapper>
     );
